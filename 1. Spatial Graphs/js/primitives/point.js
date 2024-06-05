0b6923c22a4931a6bf7bdda5744f411b249e6916
Bluecode77732 <doookuuu@gmail.com>
2024-06-05 20:33:29 +0900

    New commits

--- 1. Spatial Graphs/js/primitives/point.js
+++ 1. Spatial Graphs/js/primitives/point.js
@@ -1,3 +1,5 @@
+/* References are mostly from W3S - https://www.w3schools.com/jsref/api_canvas.asp */
+
 class Point {
     constructor(x, y) {
         this.x = x;
@@ -9,9 +11,10 @@ class Point {
     }
 
     draw(ctx, size = 18, color = "black") {
-        const rad = size / 2;
-        /* ctx.beginPath(); */    /* No declaration of 'beginPath'. */
-        
+        const radius = size / 2;
+        ctx.beginPath();    /* No declaration of 'beginPath'. W3S - beginPath() : Begins a new path or resets the current path. */
+        ctx.fillStyle = color;
+        ctx.arc(this.x, this.y, radius, 0, Math.PI * 2);   /* arc() : Adds an arc/curve (circle, or parts of a circle) to the path. */
+        ctx.fill(); /* fill() : Fills the current path */
     }
-
 }
--- 1. Spatial Graphs/js/primitives/segment.js
+++ 1. Spatial Graphs/js/primitives/segment.js
@@ -12,6 +12,12 @@ class Segment {
         return this.p1.equals(point) || this.p2.equals(point);
     }
 
-    
-
+    draw(ctx, width = 2, color = "black") {
+        ctx.beginPath();
+        ctx.lineWidth = color;
+        ctx.strokeStyle = color;
+        ctx.moveTo(this.p1.x, this.p1.y);
+        ctx.lineTo(this.p2.x, this.p2.y);
+        ctx.stroke();
+    }
 }
